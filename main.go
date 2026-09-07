51eb7aee5f7247f1282702dd1c6c76dcd291c849
V3N <125910834+V3NV@users.noreply.github.com>
2026-09-07 23:43:09 +0300

    Update main.go

--- main.go
+++ main.go
@@ -70,11 +70,18 @@ func makeRequest(ctx *fasthttp.RequestCtx, attempt int) *fasthttp.Response {
 	url := strings.SplitN(string(ctx.Request.Header.RequestURI())[1:], "/", 2)
 	req.SetRequestURI("https://" + url[0] + ".roblox.com/" + url[1])
 	req.SetBody(ctx.Request.Body())
-	ctx.Request.Header.VisitAll(func (key, value []byte) {
-		req.Header.Set(string(key), string(value))
-	})
-	req.Header.Set("User-Agent", "RoProxy")
-	req.Header.Del("Roblox-Id")
+ctx.Request.Header.VisitAll(func(key, value []byte) {
+	req.Header.Set(string(key), string(value))
+})
+
+robloxCookie := os.Getenv("ROBLOX_COOKIE")
+if robloxCookie != "" {
+	req.Header.Set("Cookie", ".ROBLOSECURITY="+robloxCookie)
+}
+
+req.Header.Set("User-Agent", "RoProxy")
+req.Header.Del("Roblox-Id")
+req.Header.Del("PROXYKEY")
 	resp := fasthttp.AcquireResponse()
 
 	err := client.Do(req, resp)
